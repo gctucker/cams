8b6aaf5d2891d5601549acd9b4addfaf445a4c44
Guillaume Tucker <guillaume.tucker@laposte.net>
2010-09-14 23:39:57 +0100

    admin: sort out lists vs tuples in parameters
    . fieldsets: list of tuples
    . lists:  fields, raw_id_fields, filter_horizontal, inlines, list_display,
              list_display_links, search_fields, list_filter
    . tuples: ordering

--- admin.py
+++ admin.py
@@ -9,14 +9,14 @@ from cams.models import (Person, Organisation, Member,
 
 class ContactInline (admin.StackedInline):
     extra = 0
-    fieldsets = (('Address',
+    fieldsets = [('Address',
                   {'fields': ('line_1', 'line_2', 'line_3',
                               ('town', 'postcode'))}),
                  ('Extra',
                   {'fields': (('telephone', 'mobile', 'fax'),
                               ('email', 'website'))}),
                  ('Order',
-                  {'fields': (('addr_order', 'addr_suborder'), )}))
+                  {'fields': (('addr_order', 'addr_suborder'), )})]
 
 
 class PersonContactInline (ContactInline):
@@ -34,42 +34,45 @@ class MemberContactInline (ContactInline):
 
 class MemberInline (admin.TabularInline):
     model = Organisation.members.through
-    fields = ('person', 'title')
-    raw_id_fields = ('person', )
+    fields = ['person', 'title']
+    raw_id_fields = ['person']
     extra = 3
 
 
 class PersonAdmin (admin.ModelAdmin):
     list_per_page = 50
     search_fields = ['first_name', 'middle_name', 'last_name', 'nickname']
-    fieldsets = ((None,
+    list_display = ['first_name', 'last_name', 'created']
+    fieldsets = [(None,
                   {'fields': ('title',
                               ('first_name', 'last_name'),
                               ('middle_name', 'nickname'),
                               'status')}),
                  ('Friends and relatives',
                   {'classes': ('collapse', ),
-                   'fields': ('alter', )}))
+                   'fields': ('alter', )})]
     radio_fields = {'title': admin.HORIZONTAL}
     filter_horizontal = ['alter']
     inlines = [PersonContactInline]
+    list_filter = ['status']
 
 
 class OrganisationAdmin (admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ['name']
     list_per_page = 50
-    search_fields = ('name', 'nickname')
-    fieldsets = ((None, {'fields': (('name', 'nickname'), 'status')}), )
+    search_fields = ['name', 'nickname']
+    fieldsets = [(None, {'fields': (('name', 'nickname'), 'status')})]
     inlines = [OrgContactInline, MemberInline]
+    list_filter = ['status']
 
 
 class MemberAdmin (admin.ModelAdmin):
-    list_display = ('person', 'organisation', 'title')
-    ordering = ['person']
+    list_display = ['person', 'organisation', 'title']
+    ordering = ('person', )
     list_per_page = 50
-    search_fields = ('person__first_name', 'person__middle_name',
+    search_fields = ['person__first_name', 'person__middle_name',
                      'person__last_name', 'person__nickname',
-                     'organisation__name', 'organisation__nickname')
+                     'organisation__name', 'organisation__nickname']
     inlines = [MemberContactInline]
     raw_id_fields = ['person', 'organisation']
 
@@ -87,46 +90,47 @@ class RoleInline (admin.TabularInline):
 
 
 class GroupAdmin (admin.ModelAdmin):
-    search_fields = ('name', 'fair__date')
+    search_fields = ['name', 'fair__date']
 
 
 class FairAdmin (admin.ModelAdmin):
-    list_display = ('date', 'description', 'current')
-    ordering = ['-date']
+    list_display = ['date', 'description', 'current']
+    ordering = ('-date', )
 
 
 class ParticipantAdmin (admin.ModelAdmin):
-    search_fields = ('person__first_name', 'person__middle_name',
-                     'person__last_name', 'person__nickname')
-    list_display = ('person', 'current_groups')
+    search_fields = ['person__first_name', 'person__middle_name',
+                     'person__last_name', 'person__nickname']
+    list_display = ['person', 'current_groups']
     list_per_page = 30
     inlines = [RoleInline]
+    list_filter = ['status']
 
 
 class EventAdmin (admin.ModelAdmin):
-    search_fields = ('name', 'org__name')
-    list_display = ('__unicode__', 'date', 'time', 'org', 'owner', 'status')
-    list_display_links = ('__unicode__', )
+    search_fields = ['name', 'org__name']
+    list_display = ['__unicode__', 'date', 'time', 'org', 'owner', 'status']
+    list_display_links = ['__unicode__']
     list_per_page = 30
-    ordering = ['-date', '-time']
-    list_filter = ('status', 'fair')
+    ordering = ('-date', '-time')
+    list_filter = ['status', 'fair']
 
 
 class ActorAdmin (admin.ModelAdmin):
-    search_fields = ('event__name', 'participant__person__first_name',
+    search_fields = ['event__name', 'participant__person__first_name',
                      'participant__person__middle_name',
                      'participant__person__last_name',
-                     'participant__person__nickname')
-    list_display = ('date', '__unicode__')
-    list_display_links = ('date', '__unicode__')
+                     'participant__person__nickname']
+    list_display = ['date', '__unicode__']
+    list_display_links = ['date', '__unicode__']
     list_per_page = 30
-    ordering = ['-event__date']
+    ordering = ('-event__date', )
     raw_id_fields = ['event', 'participant']
 
 
 class CommentAdmin (admin.ModelAdmin):
-    list_display = ('created', 'author', '__unicode__')
-    ordering = ['-created']
+    list_display = ['created', 'author', '__unicode__']
+    ordering = ('-created', )
     raw_id_fields = ['author']
 
 
